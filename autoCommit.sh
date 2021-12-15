ed6333d912667c965bc861a59109d85e70313fc5
zhou-yjc <92678817+zhou-yjc@users.noreply.github.com>
2021-12-15 21:17:48 +0800

    Update autoCommit.sh

--- autoCommit.sh
+++ autoCommit.sh
@@ -3,8 +3,8 @@
 d = `date "+现在时间是：%Y-%m-%d %H:%M:%S"`
 # d = '你好'
 echo "$d" >> ./README.md
-git fetch origin
-git merge origin/main
+git fetch https://github.com/zhou-yjc/AutoCommit.git
+git merge https://github.com/zhou-yjc/AutoCommit.git/main
 git add ./README.md
 git commit -m "$d"
 # git push origin main

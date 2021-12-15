2ff6fb38a2c91c281d4517f24c54e988ca4f7c81
zhou-yjc <92678817+zhou-yjc@users.noreply.github.com>
2021-12-15 21:06:46 +0800

    Update autoCommit.sh

--- autoCommit.sh
+++ autoCommit.sh
@@ -2,8 +2,8 @@
 
 d = `date "+%Y-%m-%d %H:%M:%S"`
 # d = '你好'
-echo "$d" >> './README.md'
+echo "$d" >> ./README.md
 git fetch origin
 git merge origin/main
-git commit ./* -m "$d"
+git commit -m "$d"
 # git push origin main

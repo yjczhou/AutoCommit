8b5dbccf9306ac237dd7ec1a887eb63a2a3670da
zhou-yjc <92678817+zhou-yjc@users.noreply.github.com>
2021-12-15 20:41:26 +0800

    Update autoCommit.sh

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-d = `TZ=Asia/Shanghai date "+%Y-%m-%d %H:%M:%S"`
+#d = `TZ=Asia/Shanghai date "+%Y-%m-%d %H:%M:%S"`
+d = '你好'
 echo "$d  " >> './README.md'
 git fetch origin
 git merge origin/main
